82ac0d637cf8679295e98b7a8c58de38d3574803
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-04 17:40:00 +0900

    회원가입 기능 추가

---
+++ src/main/java/com/gyojincompany/jpa/JpaTest20230504Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTest20230504Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTest20230504Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/jpa/JpaTest20230504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTest20230504ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
