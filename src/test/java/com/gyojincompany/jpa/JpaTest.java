a9508c0d992360ce864a592cad6d07aa39effe6f
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-10 11:17:20 +0900

    회원 탈퇴 및 전체 회원 리스트 보기 기능 추가

--- src/test/java/com/gyojincompany/jpa/JpaTest.java
+++ src/test/java/com/gyojincompany/jpa/JpaTest.java
@@ -19,8 +19,7 @@ public class JpaTest {
 	MemberRepository memberRepository;
 	
 	@Test
-	@DisplayName("이름 검색 테스트")
-	
+	@DisplayName("이름 검색 테스트")	
 	public void searchName() {
 		
 		List<MemberDto> memberDtos = memberRepository.findByName("이순신");
@@ -29,6 +28,26 @@ public class JpaTest {
 			System.out.println(memberDto.toString());
 		}
 	}
+	
+//	@Test
+//	@DisplayName("회원 탈퇴 테스트")
+//	public void deleteMember() {
+//		
+//		memberRepository.deleteById(2L);//학번이 2번 멤버 삭제
+//		
+//	}
+	
+	@Test
+	@DisplayName("회원 리스트 테스트")
+	public void memberList() {
+		List<MemberDto> memberDtos = memberRepository.findAll();//모든 회원 리스트 가져오기
+		for(MemberDto memberDto : memberDtos) {
+			System.out.println(memberDto.toString());
+		}
+	}
+	
+	
+	
 	
 
 }
